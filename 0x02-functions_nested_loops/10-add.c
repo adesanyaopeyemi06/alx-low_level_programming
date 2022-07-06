588bd7c0b06120fa7a89e5bb64c7027446e27b6e
adesanyaopeyemi06 <adesanyaopeyemi06@gmail.com>
2022-07-06 05:42:00 -0700

    wow

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  add - adds two integrers and returns the result
+ *
+ *  @x: number to be added to another
+ *
+ *  @y: number to be added to another
+ *
+ *  Return: Always 0.
+ *
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
