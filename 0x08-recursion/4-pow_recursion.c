de6536c2f7894256d0127c2d77da3683667e715f
adesanyaopeyemi06 <adesanyaopeyemi06@gmail.com>
2022-07-20 04:59:45 -0700

    five

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+o#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x index y
+ * @x: base.
+ * @y: index
+ * Return: result of the index
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
