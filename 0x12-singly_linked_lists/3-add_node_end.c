e8b669214274ef81821ccfc98e80e06f140b4b69
adesanyaopeyemi06 <adesanyaopeyemi06@gmail.com>
2022-08-10 22:55:43 -0700

    morning tea

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+
